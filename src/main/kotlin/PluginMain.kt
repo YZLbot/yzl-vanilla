82e32e2839681b4edcd97ca2b25fa247c3819370
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2024-02-01 22:01:00 +0800

    ver: 2.0.2->2.0.3

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -16,7 +16,7 @@ object PluginMain : KotlinPlugin(
     JvmPluginDescription(
         id = "top.tbpdt.vanilla",
         name = "YZLbot-Vanilla",
-        version = "2.0.2",
+        version = "2.0.3",
     ) {
 
         author("Takeoff0518")
