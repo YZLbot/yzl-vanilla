eaedff78fb35673d55dec257219fbacbaaf24dcf
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2024-02-12 14:04:19 +0800

    add: more adorable EatDots

--- src/main/kotlin/EatDots.kt
+++ src/main/kotlin/EatDots.kt
@@ -9,19 +9,22 @@ import top.tbpdt.utils.MessageUtils.getPlainText
 /**
  * @author Takeoff0518
  */
-object EatDots: SimpleListenerHost() {
+object EatDots : SimpleListenerHost() {
+    private val indexList: List<String> =
+        listOf("一", "两", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四", "十五")
+
     @EventHandler(priority = EventPriority.HIGH)
     suspend fun GroupMessageEvent.onMessage() {
         val plainText = message.getPlainText().trim()
         val dotCount = if (plainText.isNotEmpty() && plainText.all { it == '。' }) plainText.length else -1
         if (dotCount == -1) return
         if (dotCount > 15) {
-            group.sendMessage(message.quote() + "小句号……吃掉吃掉吃掉……")
+            group.sendMessage(message.quote() + "好多小句号诶……吃掉吃掉吃掉……")
             delay(1000)
             group.sendMessage("吃不了了~>_<~")
             return
         }
-        val result = "小句号……" + "吃掉".repeat(dotCount) + "~"
+        val result = indexList[dotCount - 1] + "个小句号诶……" + "吃掉".repeat(dotCount) + "~"
         group.sendMessage(message.quote() + result)
     }
 }
