b851aff6f6cd57092eaf50d88939b320c3b0b2c9
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2024-05-03 01:06:12 +0800

    better InviteProcessor message

--- src/main/kotlin/top/tbpdt/vanilla/InviteProcessor.kt
+++ src/main/kotlin/top/tbpdt/vanilla/InviteProcessor.kt
@@ -27,16 +27,16 @@ object InviteProcessor : SimpleListenerHost() {
             |来自群：${if (this.fromGroupId == 0L) "未知" else fromGroupId}
         """.trimMargin()
         if (!autoAcceptNewFriendRequest) {
-            bot.getGroup(GlobalConfig.group)?.sendMessage("按照配置，已回绝加好友请求\n$textTemplate")
+            bot.getGroup(GlobalConfig.group)?.sendMessage("按照配置，已回绝加好友请求~\n$textTemplate")
         }
         if (queryExperience(fromId) < experienceLimit) {
             bot.getGroup(GlobalConfig.group)
-                ?.sendMessage("由于经验限制 (${queryExperience(fromId)}<$experienceLimit)，已回绝加好友请求\n$textTemplate")
+                ?.sendMessage("由于经验限制 (${queryExperience(fromId)} < $experienceLimit)，已回绝加好友请求~\n$textTemplate")
             return
         }
         delay((1000L..5000L).random())
         accept()
-        bot.getGroup(GlobalConfig.group)?.sendMessage("已同意加好友请求\n$textTemplate")
+        bot.getGroup(GlobalConfig.group)?.sendMessage("已同意加好友请求~\n$textTemplate")
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -46,17 +46,17 @@ object InviteProcessor : SimpleListenerHost() {
             |目标群：${this.groupName}(${this.groupId})
         """.trimMargin()
         if (!autoAcceptInvitedJoinGroupRequest) {
-            bot.getGroup(GlobalConfig.group)?.sendMessage("按照配置，已回绝拉群请求\n$textTemplate")
+            bot.getGroup(GlobalConfig.group)?.sendMessage("按照配置，已回绝拉群请求~\n$textTemplate")
             return
         }
         if (queryExperience(invitorId) < experienceLimit) {
             bot.getGroup(GlobalConfig.group)
-                ?.sendMessage("由于经验限制 (${queryExperience(invitorId)}<$experienceLimit)，已回绝拉群请求\n$textTemplate")
+                ?.sendMessage("由于经验限制 (${queryExperience(invitorId)} < $experienceLimit)，已回绝拉群请求~\n$textTemplate")
             return
         }
         if (queryMoney(invitorId) < moneyLimit) {
             bot.getGroup(GlobalConfig.group)
-                ?.sendMessage("由于经济限制 (${queryMoney(invitorId)}<$moneyLimit)，已回绝拉群请求\n$textTemplate")
+                ?.sendMessage("由于经济限制 (${queryMoney(invitorId)} < $moneyLimit)，已回绝拉群请求~\n$textTemplate")
             return
         }
         AccountUtils.addMoney(invitorId, -moneyLimit)
@@ -64,10 +64,10 @@ object InviteProcessor : SimpleListenerHost() {
         if (group != null && invitorId in group.members) {
             delay((1000L..5000L).random())
             accept()
-            bot.getGroup(GlobalConfig.group)?.sendMessage("已同意拉群请求 (li-$moneyLimit)\n$textTemplate")
+            bot.getGroup(GlobalConfig.group)?.sendMessage("已同意拉群请求，消耗 $moneyLimit li~\n$textTemplate")
         } else if (invitorId in bot.friends) {
             bot.getFriend(invitorId)?.sendMessage("请在加群 ${GlobalConfig.group} 后再次尝试拉群~")
-            bot.getGroup(GlobalConfig.group)?.sendMessage("由于未加饲养群，已回绝拉群请求\n$textTemplate")
+            bot.getGroup(GlobalConfig.group)?.sendMessage("由于未加饲养群，已回绝拉群请求~\n$textTemplate")
         }
     }
 }
