d8bf5dae943d2d090914fa402104d22bb14afcd1
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2024-08-04 22:32:07 +0800

    combine EatDots to AutoGroup

--- src/main/kotlin/top/tbpdt/vanilla/AutoGroup.kt
+++ src/main/kotlin/top/tbpdt/vanilla/AutoGroup.kt
@@ -10,6 +10,7 @@ import net.mamoe.mirai.event.SimpleListenerHost
 import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.code.MiraiCode.deserializeMiraiCode
 import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import net.mamoe.mirai.message.data.PlainText
 import top.tbpdt.configer.AutoConfig
 import top.tbpdt.configer.AutoConfig.botUnmutedMessage
@@ -17,6 +18,7 @@ import top.tbpdt.configer.AutoConfig.defaultNewMemberJoinMessage
 import top.tbpdt.configer.AutoConfig.groupMuteAllRelease
 import top.tbpdt.configer.AutoConfig.newMemberJoinMessage
 import top.tbpdt.utils.MessageUtils.encodeToMiraiCode
+import top.tbpdt.utils.MessageUtils.getPlainText
 import top.tbpdt.vanilla.PluginMain.logger
 
 /**
@@ -109,6 +111,24 @@ object AutoGroup : SimpleListenerHost() {
             else -> At(member).plus(PlainText(" 升职啦！"))
         }
         group.sendMessage(msg)
+    }
+
+    private val indexList: List<String> =
+        listOf("一", "两", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四", "十五")
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    suspend fun GroupMessageEvent.onMessage() {
+        val plainText = message.getPlainText().trim()
+        val dotCount = if (plainText.isNotEmpty() && plainText.all { it == '。' }) plainText.length else -1
+        if (dotCount == -1) return
+        if (dotCount > 15) {
+            group.sendMessage(message.quote() + "好多小句号诶……吃掉吃掉吃掉……")
+            delay(1000)
+            group.sendMessage("吃不了了~>_<~")
+            return
+        }
+        val result = indexList[dotCount - 1] + "个小句号诶……" + "吃掉".repeat(dotCount) + "~"
+        group.sendMessage(message.quote() + result)
     }
 
 }
