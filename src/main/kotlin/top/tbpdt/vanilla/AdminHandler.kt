0c7e72b5792e12252496c4c8e97246834c6be749
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2024-08-04 22:29:31 +0800

    add: .save

--- src/main/kotlin/top/tbpdt/vanilla/AdminHandler.kt
+++ src/main/kotlin/top/tbpdt/vanilla/AdminHandler.kt
@@ -10,7 +10,10 @@ import top.tbpdt.configer.GlobalConfig
 import top.tbpdt.configer.MuteMeConfig
 import top.tbpdt.utils.MessageUtils.isCommand
 import top.tbpdt.vanilla.PluginMain.reload
+import top.tbpdt.vanilla.PluginMain.save
+import top.tbpdt.vanilla.configer.AutoPicsConfig
 import top.tbpdt.vanilla.configer.CaveConfig
+import top.tbpdt.vanilla.configer.CensorConfig
 
 /**
  * @author Takeoff0518
@@ -27,9 +30,25 @@ object AdminHandler : SimpleListenerHost() {
                 GlobalConfig.reload()
                 CaveConfig.reload()
                 MuteMeConfig.reload()
-                subject.sendMessage("重载成功~")
+                CensorConfig.reload()
+                AutoPicsConfig.reload()
+                subject.sendMessage("重载配置成功~")
             } catch (e: Exception) {
-                subject.sendMessage("重载失败！\n$e")
+                subject.sendMessage("重载配置失败！\n$e")
+            }
+        }
+        if (message.isCommand("save")) {
+            try {
+                AutoConfig.save()
+                EmojiConfig.save()
+                GlobalConfig.save()
+                CaveConfig.save()
+                MuteMeConfig.save()
+                CensorConfig.save()
+                AutoPicsConfig.save()
+                subject.sendMessage("强行保存配置成功~")
+            } catch (e: Exception) {
+                subject.sendMessage("强行保存配置失败！\n$e")
             }
         }
     }
